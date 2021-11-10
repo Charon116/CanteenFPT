9fd9d3d25ceff6914c7534db6132c106e706cfbd
Charon116 <lcquang116@gmail.com>
2021-11-10 23:17:13 +0700

    cash payment
    cash payment

--- app/src/main/java/com/example/canteenfpt/customerFoodPanel/CustomerPaymentOrders1.java
+++ app/src/main/java/com/example/canteenfpt/customerFoodPanel/CustomerPaymentOrders1.java
@@ -2,27 +2,15 @@ package com.example.canteenfpt.customerFoodPanel;
 
 public class CustomerPaymentOrders1 {
 
-    private String Address,GrandTotalPrice,MobileNumber,Name,Note,RandomUID;
+    private String GrandTotalPrice,MobileNumber,RandomUID;
 
-    public CustomerPaymentOrders1(String address, String grandTotalPrice, String mobileNumber, String name, String note, String randomUID) {
-        Address = address;
+    public CustomerPaymentOrders1(String grandTotalPrice, String mobileNumber, String randomUID) {
         GrandTotalPrice = grandTotalPrice;
         MobileNumber = mobileNumber;
-        Name = name;
-        Note = note;
         RandomUID = randomUID;
     }
 
     public CustomerPaymentOrders1() {
-
-    }
-
-    public String getAddress() {
-        return Address;
-    }
-
-    public void setAddress(String address) {
-        Address = address;
     }
 
     public String getGrandTotalPrice() {
@@ -39,22 +27,6 @@ public class CustomerPaymentOrders1 {
 
     public void setMobileNumber(String mobileNumber) {
         MobileNumber = mobileNumber;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getNote() {
-        return Note;
-    }
-
-    public void setNote(String note) {
-        Note = note;
     }
 
     public String getRandomUID() {
