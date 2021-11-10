9b3eb5da0a4cc7de7bdb5bb4727682b65b27907c
Charon116 <lcquang116@gmail.com>
2021-11-10 22:40:25 +0700

    customer payment
    customer payment

--- app/src/main/java/com/example/canteenfpt/customerFoodPanel/CustomerPayment.java
+++ app/src/main/java/com/example/canteenfpt/customerFoodPanel/CustomerPayment.java
@@ -1,4 +1,19 @@
 package com.example.canteenfpt.customerFoodPanel;
 
-public class CustomerPayment {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.canteenfpt.R;
+
+
+public class CustomerPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_payment);
+
+
+    }
 }
